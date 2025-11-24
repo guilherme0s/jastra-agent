154332ffffa2e5e063b4b8ec6e04386b4432ee76
guilherme0s <devguilheme0s@gmail.com>
2025-11-24 15:03:53 -0300

    Add Inter font integration and update styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 
+import '@fontsource/inter';
 import '@/styles/main.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
